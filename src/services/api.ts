21162f7760348add686d1f9b259f3f2f8fd956ee
Lucas Santana <57304859+LucasSantana12@users.noreply.github.com>
2022-02-17 21:16:43 -0400

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://go-restaurant-o28k55ds9-batuttas.vercel.app',
+  baseURL: 'https://go-restaurant.vercel.app',
 });
 
 export default api;
